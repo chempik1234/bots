f3aee75d39f6501e303924d3bf46e0d8ea049c33
chempik1234 <dandijar@yandex.ru>
2022-03-22 20:42:49 +0500

    bot big bro

--- vk.py
+++ vk.py
@@ -1,36 +1,28 @@
 import vk_api
-import os
-
-
-def auth_handler():
-    """ При двухфакторной аутентификации вызывается эта функция. """
-
-    # Код двухфакторной аутентификации,
-    # который присылается по смс или уведомлением в мобильное приложение
-    key = input("Enter authentication code: ")
-    # Если: True - сохранить, False - не сохранять.
-    remember_device = True
-
-    return key, remember_device
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import random
 
 
 def main():
-    login, password = 'dandijar@yandex.ru', 'VKdan!S.143'
-    vk_session = vk_api.VkApi(login, password, auth_handler=auth_handler)
-    try:
-        vk_session.auth(token_only=True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-    upload = vk_api.VkUpload(vk_session)
-    photo = upload.photo([os.path.join('static\\img', 'img1.jpg'),
-                          os.path.join('static\\img', 'img2.jpg')], '283448220', group_id='212025650')
+    vk_session = vk_api.VkApi(
+        token='0d89314c6e248981a5ace7e252eca33d650f91fef28ed6025675840cb45f450faea53fe0742ea7f5cb0ea')
 
-    vk_photo_id = f"photo{photo[0]['owner_id']}_{photo[0]['id']}"
+    longpoll = VkBotLongPoll(vk_session, '212025650')
 
-    print(photo, vk_photo_id, sep="\n")
-    vk = vk_session.get_api()
-    vk.wall.post(message="Test", attachments=[vk_photo_id])
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            print(event)
+            print('Новое сообщение:')
+            print('Для меня от:', event.obj.message['from_id'])
+            print('Текст:', event.obj.message['text'])
+            vk = vk_session.get_api()
+            users = vk.users.get(user_ids=event.obj.message['from_id'], fields='first_name,city')[0]
+            text = 'Привет, ' + users['first_name'] + '!'
+            if users.get('city'):
+                text += ' Как поживает ' + users['city']['title'] + '?'
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message=text,
+                             random_id=random.randint(0, 2 ** 64))
 
 
 if __name__ == '__main__':
