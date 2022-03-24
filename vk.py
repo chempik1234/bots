a010a76c891c0f4bc9af787ecebbfc00dfb5e78e
chempik1234 <dandijar@yandex.ru>
2022-03-24 09:36:20 +0500

    wiki

--- vk.py
+++ vk.py
@@ -1,6 +1,6 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-import random
+import random, wikipedia
 
 
 def auth_handler():
@@ -16,35 +16,47 @@ def auth_handler():
 
 
 def main():
-    login, password = 'dandijar@yandex.ru', 'VKdan!S.143'
-    vk_session = vk_api.VkApi(login, password, auth_handler=auth_handler)
-    try:
-        vk_session.auth(token_only=True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-
-    vk = vk_session.get_api()
-    ids = ['photo' + str(i['owner_id']) + '_' + str(i['id'])
-           for i in vk.photos.get(group_id='212025650', album_id='283448220')['items']]
-    vk_session = vk_api.VkApi(token='0d89314c6e248981a5ace7e252eca33d650f91fef28ed6025675840cb45f450faea53fe0742ea7f5cb0ea')
+    cyrillic = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+    vk_session = vk_api.VkApi(token='0d89314c6e248981a5ace7e252eca33d650f91fef28ed'
+                                    '6025675840cb45f450faea53fe0742ea7f5cb0ea')
     vk = vk_session.get_api()
 
     longpoll = VkBotLongPoll(vk_session, '212025650')
 
     for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
+        if event.type == VkBotEventType.GROUP_JOIN:
+            print(event)
+            user = vk.users.get(user_ids=event.obj.message['from_id'], fields='first_name')[0]
+            text = 'Привет, ' + user['first_name'] + '! Задавайте вопросы.'
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message=text,
+                             random_id=random.randint(0, 2 ** 64))
+        elif event.type == VkBotEventType.MESSAGE_NEW:
             print(event)
             print('Новое сообщение:')
             print('Для меня от:', event.obj.message['from_id'])
             print('Текст:', event.obj.message['text'])
-            vk = vk_session.get_api()
-            users = vk.users.get(user_ids=event.obj.message['from_id'], fields='first_name')[0]
-            text = 'Привет, ' + users['first_name'] + '!'
-            vk.messages.send(user_id=event.obj.message['from_id'],
-                             message=text,
-                             attachment=random.choice(ids),
-                             random_id=random.randint(0, 2 ** 64))
+            message = event.obj.message['text']
+            if any(i in cyrillic for i in message.lower()):
+                wikipedia.set_lang('ru')
+            else:
+                wikipedia.set_lang('en')
+            response = wikipedia.search(message)
+            text = "k"
+            if response:
+                page = wikipedia.page(response[0])
+                text = page.content
+            else:
+                text = "Википедия не ответила"
+            cur = ""
+            for i in range(len(text)):
+                if i == 4095:
+                    vk.messages.send(user_id=event.obj.message['from_id'],
+                                     message=cur,
+                                     random_id=random.randint(0, 2 ** 64))
+                    cur = ""
+                else:
+                    cur += text[i]
 
 
 if __name__ == '__main__':
