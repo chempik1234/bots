4052c12227dfbf54d1700c7f29352b7b50e5d1cd
chempik1234 <dandijar@yandex.ru>
2022-03-26 15:20:31 +0500

    test

--- tg.py
+++ tg.py
@@ -1,66 +1,38 @@
 from telegram.ext import CommandHandler, ConversationHandler
 from telegram.ext import Updater, MessageHandler, Filters
-text = 'Черемуха душистая\n' \
-       'С весною расцвела\n' \
-       'И ветки золотистые,\n' \
-       'Что кудри, завила.\n' \
-       'Кругом роса медвяная\n' \
-       'Сползает по коре,\n' \
-       'Под нею зелень пряная\n' \
-       'Сияет в серебре.\n' \
-       'А рядом, у проталинки,\n' \
-       'В траве, между корней,\n' \
-       'Бежит, струится маленький\n' \
-       'Серебряный ручей.\n' \
-       'Черемуха душистая,\n' \
-       'Развесившись, стоит,\n' \
-       'А зелень золотистая\n' \
-       'На солнышке горит.\n' \
-       'Ручей волной гремучею\n' \
-       'Все ветки обдает\n' \
-       'И вкрадчиво под кручею\n' \
-       'Ей песенки поет.'
-strings = text.split('\n')
+import json
+from random import shuffle
+with open('questions.json', 'r', encoding="UTF-8") as f:
+    file = json.load(f)['test']
 
 
 def start(update, context):
-    update.message.reply_text('Я читаю первую строчку стихотворения, а ты - вторую,'
-                              ' затем я - третью, ты - четвёртую и так до конца.\n'
-                              '/stop - прервать работу\n/suphler - подсказка\n'
-                              'Стихотворение Есенина "Черёмуха"\n'
+    update.message.reply_text('Сейчас я задам вам вопросы, а вы на них должны ответить."\n'
     )
-    context.user_data['string'] = 0
-    return string_read(update, context)
+    context.user_data['questions'] = file
+    shuffle(context.user_data['questions'])
+    context.user_data['current'] = 0
+    context.user_data['right'] = 0
+    return question(update, context)
 
 
-def suphler(update, context):
-    update.message.reply_text('Подсказка: ' + strings[context.user_data['string']])
-
-
-def string_read(update, context):
-    n = context.user_data['string']
-    update.message.reply_text(strings[n])
-    context.user_data['string'] += 1
-    return 2 if context.user_data['string'] != len(strings) else exit_(update, context)
-
-
-def string_check(update, context):
-    if update.message.text == '/suphler':
-        return suphler(update, context)
-    if update.message.text == '/stop':
-        return stop(update, context)
-    n = context.user_data['string']
-    if update.message.text != strings[n]:
-        update.message.reply_text('Нет, не так!')
-        suphler(update, context)
-        return 2
-    context.user_data['string'] += 1
-    return string_read(update, context) if context.user_data['string'] != len(strings) else exit_(update,
-                                                                                                  context)
+def question(update, context):
+    n = context.user_data['current']
+    questions = context.user_data['questions']
+    if n > 0 and questions[n - 1]['response'] == update.message.text:
+        context.user_data['right'] += 1
+    if n < len(questions):
+        update.message.reply_text('Вопрос: ' + questions[n]['question'])
+    context.user_data['current'] += 1
+    if context.user_data['current'] <= len(questions):
+        return 1
+    else:
+        return exit_(update, context)
 
 
 def exit_(update, context):
-    update.message.reply_text("Круто! Это была последняя строчка, работа окончена.")
+    update.message.reply_text("Правильных: " + str(context.user_data['right']) + '/' +
+                              str(len(context.user_data['questions'])) + '.\nЗаново: /start')
     return ConversationHandler.END
 
 
@@ -76,16 +48,14 @@ def main():
         entry_points=[CommandHandler('start', start)],
 
         states={
-            1: [MessageHandler(Filters.text, string_read, pass_user_data=True)],
-            2: [MessageHandler(Filters.text, string_check, pass_user_data=True)],
-            3: [MessageHandler(Filters.text, exit_)]
+            1: [MessageHandler(Filters.text, question, pass_user_data=True)],
+            2: [MessageHandler(Filters.text, exit_, pass_user_data=True)]
         },
 
         # Без изменений
         fallbacks=[CommandHandler('stop', stop)]
     )
     dp.add_handler(conv_handler)
-    dp.add_handler(CommandHandler('suphler', suphler, pass_user_data=True))
     updater.start_polling()
     updater.idle()
 
