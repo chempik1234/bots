5b4b00e9988ed410387a8364302c703b820fd772
chempik1234 <dandijar@yandex.ru>
2022-03-27 11:59:44 +0500

    translator

--- tg.py
+++ tg.py
@@ -1,68 +1,42 @@
 from telegram.ext import CommandHandler, ConversationHandler
 from telegram.ext import Updater, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup
 import requests
 
 
-def find_coords(geocode):
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-
-    geocoder_params = {
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": geocode,
-        "format": "json"}
-    response = requests.get(geocoder_api_server, params=geocoder_params)
-    json_response = response.json()
-    if not response or not json_response["response"]["GeoObjectCollection"]["featureMember"]:
-        return False
-    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-    toponym_coordinates = toponym["Point"]["pos"]
-    return toponym_coordinates.replace(' ', ',')
-
-
-def get_size(address):
-    search_api_server = "https://search-maps.yandex.ru/v1/"
-    api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
-    search_params = {"apikey": api_key, "lang": "ru_RU", "text": address}
-    response = requests.get(search_api_server, params=search_params)
-    json_response = response.json()
-    organization = json_response["features"][0]["properties"]
-    toponym_lc, toponym_uc = organization["boundedBy"]
-    toponym_size = max(abs(toponym_lc[0] - toponym_uc[0]),
-                       abs(toponym_lc[1] - toponym_uc[1]))
-    return str(toponym_size) + ',' + str(toponym_size)
-
-
-def geocoder(update, context):
+def translator(update, context):
+    reply_keyboard = [['en|ru', 'ru|en']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     try:
-        geocoder_uri = geocoder_request_template = "http://geocode-maps.yandex.ru/1.x/"
-        response = requests.get(geocoder_uri, params={
-            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-            "format": "json",
-            "geocode": update.message.text
-        })
-        ll, spn = find_coords(update.message.text), get_size(update.message.text)
-        static_api_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&spn={spn}&l=map&pt={ll},pm2vvm"
-        context.bot.send_photo(
-            update.message.chat_id,  # Идентификатор чата. Куда посылать картинку.
-            # Ссылка на static API, по сути, ссылка на картинку.
-            # Телеграму можно передать прямо её, не скачивая предварительно карту.
-            static_api_request,
-            caption="Нашёл:"
-        )
+        if not context.user_data.get('langpair'):
+            context.user_data['langpair'] = "ru|en"
+        if update.message.text == 'en|ru':
+            context.user_data['langpair'] = "en|ru"
+            return
+        elif update.message.text == 'ru|en':
+            context.user_data['langpair'] = "ru|en"
+            return
+        lang = context.user_data['langpair']
+        word = update.message.text
+        url = "https://translated-mymemory---translation-memory.p.rapidapi.com/api/get"
+        querystring = {"langpair": lang, "q": word}
+        headers = {
+            'x-rapidapi-key': "24fd2ead8amshc203cd479b8fa8bp18d9b1jsnb8779c61c6a3",
+            'x-rapidapi-host': "translated-mymemory---translation-memory.p.rapidapi.com"
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        output = response.json()
+        new = output['responseData']['translatedText']
+        update.message.reply_text(new, reply_markup=markup)
     except Exception as err:
-        d = {'list index out of range': 'Ничего не найдено',
-             'Failed to get http url content': 'Ничего не найдено',
-             'Wrong file identifier/http url specified': 'Ничего не найдено'}
-        text = err.__str__()
-        if text in d.keys():
-            text = d[text]
-        update.message.reply_text('Ошибка ' + err.__class__.__name__.__str__() + ': ' + text)
+        update.message.reply_text('Ошибка ' + err.__class__.__name__.__str__() + ': ' + err.__str__(),
+                                  reply_markup=markup)
 
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater('5147513805:AAEiG0-XjDlug2pmoPcRPuufLSZBYs8jbIc', use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text, geocoder))
+    dp.add_handler(MessageHandler(Filters.text, translator, pass_user_data=True))
     updater.start_polling()
     updater.idle()
 
