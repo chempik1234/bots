874fc548bd262a05357efff56436b1c32047be19
chempik1234 <dandijar@yandex.ru>
2022-03-27 13:59:19 +0500

    timer

--- discord_.py
+++ discord_.py
@@ -1,15 +1,7 @@
+import asyncio
 import discord
-import requests
-cats = ["кот", "котик", "кошка", "кошечка", "коты", "котики", "кошки", "кошечки",
-        "котёнок", "котенок", "котятки"]
-dogs = ["пес", "пёс", "песик", "пёсик", "собака", "собачка",
-        "псы", "пёсики", "собаки", "собачки",
-        "щенок", "щенки", "щеночек", "щеночки"]
+
 TOKEN = None
-
-
-def clean_str(a):
-    return ''.join(i for i in a if i not in ',./\\!@"№#$;%^:&?*()~`' + "'<>-+=_")
 
 
 class YLBotClient(discord.Client):
@@ -17,7 +9,7 @@ class YLBotClient(discord.Client):
         print(f'{self.user} has connected to Discord!')
         for guild in self.guilds:
             for channel in guild.text_channels:
-                await channel.send('Могу отправить картинку с котиком или собачкой!')
+                await channel.send('set_timer in X hours Y minutes')
             print(
                 f'{self.user} подключились к чату:\n'
                 f'{guild.name}(id: {guild.id})')
@@ -25,11 +17,13 @@ class YLBotClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if any(i in clean_str(message.content.lower()) for i in cats):
-            await message.channel.send(requests.get("https://api.thecatapi.com/v1/images/search?format=json").json()[0]
-                                       ['url'])
-        elif any(i in clean_str(message.content.lower()) for i in dogs):
-            await message.channel.send(requests.get("https://dog.ceo/api/breeds/image/random").json()["message"])
+        args = message.content.split()
+        if len(args) == 6 and [args[0], args[1], args[3], args[5]] == ["set_timer", "in", "hours", "minutes"] and \
+                args[2].isdigit() and args[4].isdigit():
+            seconds = int(args[2]) * 60 * 60 + int(args[4]) * 60
+            await message.channel.send("The timer should start in " + args[2] + " hours " + args[4] + " minutes")
+            await asyncio.sleep(seconds)
+            await message.channel.send(":alarm_clock:Time X has come!")
 
 
 client = YLBotClient()
